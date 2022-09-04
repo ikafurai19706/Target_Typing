0c17fd315183b7a6493bfc00d166ee6e46aec8e4
ikafu <ikafu@DESKTOP-V00BMES>
2022-09-04 23:56:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Target_Typing.py
@@ -0,0 +1,24 @@
+﻿# coding: utf-8
+import multiprocessing as mprocess
+from tkinter import *
+from tkinter import ttk
+
+def SetDisplayPos():
+    Swidth = root.winfo_screenwidth()
+    Sheight = root.winfo_screenheight()
+    Wwidth = Swidth//2 - 320
+    Wheight = Sheight//2 - 240
+    return '640x480+'+str(Wwidth)+'+'+str(Wheight)
+
+root = Tk()
+root.title('Target_Typing')
+root.geometry(SetDisplayPos())
+root.resizable(False, False)
+
+button = ttk.Button(root,text='button')
+label1 = ttk.Label(root,text='label')
+
+button.pack(side='bottom')
+label1.pack()
+
+root.mainloop()
