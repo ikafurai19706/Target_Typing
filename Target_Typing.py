98b9677cad781deffa91980a89a7dc0bc721d6bd
ikafurai19706 <ikafurai19706@gmail.com>
2022-10-20 23:46:25 +0900

    Updated Target_Typing

--- Target_Typing.py
+++ Target_Typing.py
@@ -13,6 +13,10 @@ with open("1400-test.txt", "rb") as checksum:
     if hashlib.md5(checksum.read()).hexdigest() != "4c8e5ddbcda1bf1072945106ceaa2a19":
         messagebox.showerror("Integrity error", "File data is corrupted.")
         sys.exit()
+with open("1900.txt", "rb") as checksum:
+    if hashlib.md5(checksum.read()).hexdigest() != "1f129ef0fc9d12f725eec6a7d79ea05c":
+        messagebox.showerror("Integrity error", "File data is corrupted.")
+        sys.exit()
 
 
 def DisplayPos():
--- convert-md5.py
+++ convert-md5.py
@@ -2,3 +2,5 @@
 import hashlib
 with open('1400-test.txt', 'rb') as checksum:
     print(hashlib.md5(checksum.read()).hexdigest())
+with open('1900.txt', 'rb') as checksum:
+    print(hashlib.md5(checksum.read()).hexdigest())
